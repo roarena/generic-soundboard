76edf6982f9db8d62ad5f429b1b692e268f4a965
Rodrigo Câmara <rodrigoarenacamara@gmail.com>
2018-02-01 23:51:52 -0200

    Adding more sounds
    Signed-off-by: Rodrigo Câmara <rodrigoarenacamara@gmail.com>

--- app/src/main/java/eu/rodrigocamara/genericsoundboard/data/local/SoundProfileLocalDataSource.java
+++ app/src/main/java/eu/rodrigocamara/genericsoundboard/data/local/SoundProfileLocalDataSource.java
@@ -40,7 +40,7 @@ public class SoundProfileLocalDataSource implements SoundDataSource {
                 "Sprinter Azul/Vermelha",
                 "https://pbs.twimg.com/profile_images/800701598582407168/3dKHpeEJ.jpg",
                 "http://localizei.blob.core.windows.net/anuncio/237817_FSR-4747_01.jpg",
-                3,
+                6,
                 createRogerinhoList());
         soundProfileList.add(soundProfile);
         soundProfile = new Profile(1,
@@ -59,7 +59,7 @@ public class SoundProfileLocalDataSource implements SoundDataSource {
                 "Sprinter Branca",
                 "https://pbs.twimg.com/profile_images/800892278470537216/eejoSRRh.jpg",
                 "https://http2.mlstatic.com/sprinter-313-cdi-van-executiva-16-lugares-2005-05-branca--D_NQ_NP_19850-MLB20178994616_102014-F.jpg",
-                2,
+                5,
                 createJulinhoList());
         soundProfileList.add(soundProfile);
         soundProfile = new Profile(3,
@@ -69,7 +69,7 @@ public class SoundProfileLocalDataSource implements SoundDataSource {
                 "Kombi Branca 94",
                 "https://media.tenor.com/images/78f66e06e01627aa1720e24e2b52f16c/tenor.png",
                 "http://2.bp.blogspot.com/-m-HrWdcBgRg/VqGnYjI1HcI/AAAAAAAAFYI/5A2WuIpNPfI/s1600/Kombi%2BStandard.jpg",
-                1,
+                2,
                 createMaurilioList());
         soundProfileList.add(soundProfile);
 
@@ -92,6 +92,8 @@ public class SoundProfileLocalDataSource implements SoundDataSource {
         List<Sound> soundList = new ArrayList<>();
         Sound sound = new Sound(0, "Boa noite amantes do cinema...", R.raw.maurilio_boa_noite, "https://media.tenor.com/images/78f66e06e01627aa1720e24e2b52f16c/tenor.png");
         soundList.add(sound);
+        sound = new Sound(1, "A justiça é muito injustça", R.raw.maurilio_justica, "https://media.tenor.com/images/78f66e06e01627aa1720e24e2b52f16c/tenor.png");
+        soundList.add(sound);
         return soundList;
     }
 
@@ -100,6 +102,12 @@ public class SoundProfileLocalDataSource implements SoundDataSource {
         Sound sound = new Sound(0, "Boa noite companheiros motoristas...", R.raw.julinho_seriedade, "https://pbs.twimg.com/profile_images/800892278470537216/eejoSRRh.jpg");
         soundList.add(sound);
         sound = new Sound(1, "Me da aquela moralzinha?", R.raw.julinho_moralzinha, "https://pbs.twimg.com/profile_images/800892278470537216/eejoSRRh.jpg");
+        soundList.add(sound);
+        sound = new Sound(2, "Vou ser obrigado a concordar com o...", R.raw.julinho_concordar, "https://pbs.twimg.com/profile_images/800892278470537216/eejoSRRh.jpg");
+        soundList.add(sound);
+        sound = new Sound(3, "Todo mundo sabe que cinema é...", R.raw.julinho_explosao, "https://pbs.twimg.com/profile_images/800892278470537216/eejoSRRh.jpg");
+        soundList.add(sound);
+        sound = new Sound(4, "Falo com tranquilidade", R.raw.julinho_tranquilidade, "https://pbs.twimg.com/profile_images/800892278470537216/eejoSRRh.jpg");
         soundList.add(sound);
         return soundList;
     }
@@ -111,6 +119,12 @@ public class SoundProfileLocalDataSource implements SoundDataSource {
         sound = new Sound(1, "Encontrou porra!", R.raw.rogerinho_encontrou, "https://pbs.twimg.com/profile_images/800701598582407168/3dKHpeEJ.jpg");
         soundList.add(sound);
         sound = new Sound(2, "Otário!", R.raw.rogerinho_otario, "https://pbs.twimg.com/profile_images/800701598582407168/3dKHpeEJ.jpg");
+        soundList.add(sound);
+        sound = new Sound(3, "Tem que acabar a justiça!", R.raw.rogerinho_acabar_justica, "https://pbs.twimg.com/profile_images/800701598582407168/3dKHpeEJ.jpg");
+        soundList.add(sound);
+        sound = new Sound(4, "Você é um guerreiro!", R.raw.rogerinho_guerreiro_renan, "https://pbs.twimg.com/profile_images/800701598582407168/3dKHpeEJ.jpg");
+        soundList.add(sound);
+        sound = new Sound(5, "Hulk quiser agredir uma idosa...!", R.raw.rogerinho_hulk_idosa, "https://pbs.twimg.com/profile_images/800701598582407168/3dKHpeEJ.jpg");
         soundList.add(sound);
         return soundList;
     }
